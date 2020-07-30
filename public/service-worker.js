9df738c6387b3945d8b10aaa51fc19ac8d0f046d
unknown <jonafi@gmail.com>
2020-07-29 22:12:23 -0500

    alternative service worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,31 +1,112 @@
-self.addEventListener('install', function (event) {
-    event.waitUntil(
-        caches.open('v1').then(function (cache) {
-            return cache.addAll([
-                '/icons/144x144.png',
-                '/icons/200x200.png',
-                'db.js',
-                'index.html',
-                '/',
-                'index.js',
-                'styles.css'
-            ]);
+// self.addEventListener('install', function (event) {
+//     event.waitUntil(
+//         caches.open('v1').then(function (cache) {
+//             return cache.addAll([
+//                 '/icons/144x144.png',
+//                 '/icons/200x200.png',
+//                 'db.js',
+//                 'index.html',
+//                 '/',
+//                 'index.js',
+//                 'styles.css'
+//             ]);
+//         })
+//     );
+// });
+
+// self.addEventListener('fetch', function (event) {
+//     event.respondWith(caches.match(event.request).then(function (response) {
+//         if (response !== undefined) {
+//             return response;
+//         } else {
+//             return fetch(event.request).then(function (response) {
+//                 let responseClone = response.clone();
+//                 caches.open('v1').then(function (cache) {
+//                     cache.put(event.request, responseClone);
+//                 });
+//                 return response;
+//             })
+//         }
+//     }));
+// });
+
+
+
+
+const FILES_TO_CACHE = [
+    '/icons/144x144.png',
+    '/icons/200x200.png',
+    'db.js',
+    'index.html',
+    '/',
+    'index.js',
+    'styles.css',
+    'service-worker.js'
+];
+
+const CACHE_NAME = "static-cache-v2";
+const DATA_CACHE_NAME = "data-cache-v1";
+
+// install
+self.addEventListener("install", function (evt) {
+    evt.waitUntil(
+        caches.open(CACHE_NAME).then(cache => {
+            console.log("Your files were pre-cached successfully!");
+            return cache.addAll(FILES_TO_CACHE);
+        })
+    );
+
+    self.skipWaiting();
+});
+
+self.addEventListener("activate", function (evt) {
+    evt.waitUntil(
+        caches.keys().then(keyList => {
+            return Promise.all(
+                keyList.map(key => {
+                    if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
+                        console.log("Removing old cache data", key);
+                        return caches.delete(key);
+                    }
+                })
+            );
+        })
+    );
+
+    self.clients.claim();
+});
+
+// fetch
+self.addEventListener("fetch", function (evt) {
+    // cache successful requests to the API
+    if (evt.request.url.includes("/api/")) {
+        evt.respondWith(
+            caches.open(DATA_CACHE_NAME).then(cache => {
+                return fetch(evt.request)
+                    .then(response => {
+                        // If the response was good, clone it and store it in the cache.
+                        if (response.status === 200) {
+                            cache.put(evt.request.url, response.clone());
+                        }
+
+                        return response;
+                    })
+                    .catch(err => {
+                        // Network request failed, try to get it from the cache.
+                        return cache.match(evt.request);
+                    });
+            }).catch(err => console.log(err))
+        );
+
+        return;
+    }
+
+    // if the request is not for the API, serve static assets using "offline-first" approach.
+    // see https://developers.google.com/web/fundamentals/instant-and-offline/offline-cookbook#cache-falling-back-to-network
+    evt.respondWith(
+        caches.match(evt.request).then(function (response) {
+            return response || fetch(evt.request);
         })
     );
 });
 
-self.addEventListener('fetch', function (event) {
-    event.respondWith(caches.match(event.request).then(function (response) {
-        if (response !== undefined) {
-            return response;
-        } else {
-            return fetch(event.request).then(function (response) {
-                let responseClone = response.clone();
-                caches.open('v1').then(function (cache) {
-                    cache.put(event.request, responseClone);
-                });
-                return response;
-            })
-        }
-    }));
-});
